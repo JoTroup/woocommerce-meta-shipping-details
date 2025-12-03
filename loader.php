48f44d0e43eba0c8e179eb0e284e51de3ee90a0c
JoTroup <97867298+JoTroup@users.noreply.github.com>
2025-12-03 12:22:05 +1030

    Update loader.php

--- loader.php
+++ loader.php
@@ -28,8 +28,14 @@ function modify_cart( $cart_object ) {
 		// Log the entire cart item array
 		error_log("Cart Item [$cart_item_key]: ");
 		$ccb_calculator = isset($cart_item['ccb_calculator']) ? $cart_item['ccb_calculator'] : null;
+
 		$calc_height = $quantity_field_id_5 = isset($ccb_calculator['calc_data']['quantity_field_id_5']['value']) ? $ccb_calculator['calc_data']['quantity_field_id_5']['value'] : null;
+
 		$calc_width = $quantity_field_id_4 = isset($ccb_calculator['calc_data']['quantity_field_id_4']['value']) ? $ccb_calculator['calc_data']['quantity_field_id_4']['value'] : null;
+
+		$calc_length = $quantity_field_id_6 = isset($ccb_calculator['calc_data']['quantity_field_id_10']['value']) ? $ccb_calculator['calc_data']['quantity_field_id_10']['value'] : null;
+
+		$calc_weight = $quantity_field_id_3 = isset($ccb_calculator['calc_data']['quantity_field_id_17']['value']) ? $ccb_calculator['calc_data']['quantity_field_id_17']['value'] : null;
 
 		if ($calc_height !== null && $calc_width !== null) {
 			// Example modification: Log the calculated dimensions
