f8de71ea00f264e7fc6e029f88c6ded10b41d039
JoTroup <97867298+JoTroup@users.noreply.github.com>
2025-12-03 10:57:00 +1030

    Update loader.php

--- loader.php
+++ loader.php
@@ -22,19 +22,13 @@ function modify_cart( $cart_object ) {
     if ( (is_admin() && ! defined( 'DOING_AJAX' ) ) || $cart_object->is_empty() )
         return;
 
-    foreach ( $cart_object->get_cart() as $cart_item ) {
+	foreach ( $cart_object->get_cart() as $cart_item_key => $cart_item ) {
+		// Log the entire cart item array
+		error_log("Cart Item [$cart_item_key]: " . print_r($cart_item, true));
 
-        $product_id = $cart_item['product_id'];
-
-        $data = get_post_meta( $product_id, 'ccb_calculator'); 
-
-		error_log('$data: ' . print_r( $data, true ) );
-        // $cart_item['data']->set_width( $data['shipping_width'] );
-        // $cart_item['data']->set_height( $data['shipping_width'] );
-        // $cart_item['data']->set_length( $data['shipping_length'] );
-
-        // if( !empty( $cart_item['data']->get_changes() ) )
-        //    $cart_item['data']->apply_changes();
-
-    }
+		// Log cart item data object meta (if needed)
+		if (isset($cart_item['data']) && is_object($cart_item['data'])) {
+			error_log("Cart Item Data [$cart_item_key]: " . print_r($cart_item['data']->get_data(), true));
+		}
+	}
 }
