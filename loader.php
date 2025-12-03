8adc893c0e7647bd8c3cf9c00f1f9147a14ad7e5
JoTroup <97867298+JoTroup@users.noreply.github.com>
2025-12-03 11:36:59 +1030

    Update loader.php

--- loader.php
+++ loader.php
@@ -28,7 +28,11 @@ function modify_cart( $cart_object ) {
 		// Log the entire cart item array
 		error_log("Cart Item [$cart_item_key]: ");
 		$ccb_calculator = isset($cart_item['ccb_calculator']) ? $cart_item['ccb_calculator'] : null;
-		error_log("Cart Item CCB Calculator [$cart_item_key]: " . print_r($ccb_calculator, true));
+		$calc_height = $quantity_field_id_5 = isset($ccb_calculator['calc_data']['quantity_field_id_5']) ? $ccb_calculator['calc_data']['quantity_field_id_5'] : null;
+		$calc_width = $quantity_field_id_4 = isset($ccb_calculator['calc_data']['quantity_field_id_4']) ? $ccb_calculator['calc_data']['quantity_field_id_4'] : null;
+
+		error_log(" - calc_height (quantity_field_id_5): " . print_r($calc_height, true));
+		error_log(" - calc_width (quantity_field_id_4): " . print_r($calc_width, true));
 
 
 		// Log cart item data object meta (if needed)
