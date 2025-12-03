40f3f7e860cfe270659edf7b898cb429bb650c83
JoTroup <97867298+JoTroup@users.noreply.github.com>
2025-12-03 11:38:54 +1030

    Update loader.php

--- loader.php
+++ loader.php
@@ -28,8 +28,8 @@ function modify_cart( $cart_object ) {
 		// Log the entire cart item array
 		error_log("Cart Item [$cart_item_key]: ");
 		$ccb_calculator = isset($cart_item['ccb_calculator']) ? $cart_item['ccb_calculator'] : null;
-		$calc_height = $quantity_field_id_5 = isset($ccb_calculator['calc_data']['quantity_field_id_5']) ? $ccb_calculator['calc_data']['quantity_field_id_5'] : null;
-		$calc_width = $quantity_field_id_4 = isset($ccb_calculator['calc_data']['quantity_field_id_4']) ? $ccb_calculator['calc_data']['quantity_field_id_4'] : null;
+		$calc_height = $quantity_field_id_5 = isset($ccb_calculator['calc_data']['quantity_field_id_5']['value']) ? $ccb_calculator['calc_data']['quantity_field_id_5']['value'] : null;
+		$calc_width = $quantity_field_id_4 = isset($ccb_calculator['calc_data']['quantity_field_id_4']['value']) ? $ccb_calculator['calc_data']['quantity_field_id_4']['value'] : null;
 
 		error_log(" - calc_height (quantity_field_id_5): " . print_r($calc_height, true));
 		error_log(" - calc_width (quantity_field_id_4): " . print_r($calc_width, true));
